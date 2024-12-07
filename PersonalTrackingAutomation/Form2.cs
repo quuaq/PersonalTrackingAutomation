c4f8e79c2f736e3ae3d7e323cb136674c2bdd3f0
Samet Barış Öncül <tekerlekci145@hotmail.com>
2024-12-07 19:08:41 +0200

    örnek pushlama

--- PersonalTrackingAutomation/Form2.cs
+++ PersonalTrackingAutomation/Form2.cs
@@ -17,6 +17,7 @@ namespace PersonalTrackingAutomation
             InitializeComponent();
         }
 
+        //Github puss örneği
         string connectionString = "Host=localhost;Port5432;Database=PersonalTrackingAutomation;Username=postgres;Password=123456";
 
         private void label1_Click(object sender, EventArgs e)
